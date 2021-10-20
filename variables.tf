ff8dc264c4dcaafee1cef6a9a21b03813a1e4d7a
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-10-20 12:43:40 +0530

    Update

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "domain_name" {
+  type    = string
+}
+
+variable "locations" {
+  
+  description = "locations"
+  type        = set(string)
+  default     = []
+}
+
+variable "reserved_ip_range" {
+
+    description = "reserved IP range"
+    type    = string
+}
+
+variable "authorized_networks" {
+  
+  description = "List of authorized Networks"
+  type        = set(string)
+  default     = []
+}
+
+variable "target_domain_name" {
+  type    = string
+
+}
+variable "target_dns_ip_addresses" {
+  
+  description = "Target DNS IP addresses"
+  type        = set(string)
+  default     = []
+}
+
+variable "trust_handshake_secret" {
+
+  description = "Trust Handshake Secret"
+  type        = set(string)
+  sensitive   = true
+
+}
